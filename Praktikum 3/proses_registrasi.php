2cac44178bb772b5e24cf0835fae1b4cec9a1881
Muhammad Dzaky Dzulfikar Salim <114467962+DzakyDzulfikar@users.noreply.github.com>
2023-03-10 15:53:11 +0700

    Add files via upload

---
+++ Praktikum 3/proses_registrasi.php
@@ -0,0 +1,21 @@
+<?php 
+
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang"];
+
+$program_studi = [
+    "Teknik Informatika"=>"Teknik Informatika",
+    "Sistem Informasi"=>"Sistem Informasi",
+    "Bisnis Digital"=>"Bisnis Digital"
+];
+
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWD Bootstrap"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50
+];
+
+?>
